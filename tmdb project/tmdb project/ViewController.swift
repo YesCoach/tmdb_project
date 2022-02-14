e4c6aad4da8938059150c7d9dc1d1d6c9a404684
YesCoach <sbsii1020@gmail.com>
2022-02-14 17:05:41 +0900

    docs: Project 생성 Xcode project 생성

---
+++ tmdb project/tmdb project/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  tmdb project
+//
+//  Created by 박태현 on 2022/02/14.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
